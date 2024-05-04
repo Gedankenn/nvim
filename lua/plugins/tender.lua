3da7e7602bb8092f5ad19eeaca78dac397a2f49b
Fabio <gedankenn@gmail.com>
2024-05-04 08:55:19 -0300

    first commit

---
+++ lua/plugins/tender.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "jacoborus/tender.vim",
+    lazy = false,
+    name = "tender",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "tender"
+    end
+  }
+}
