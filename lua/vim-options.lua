21d35f54dc1078349287618e9ee21a7793ee5067
FabioStella <gedankenn@gmail.com>
2024-09-21 22:27:51 -0300

    add terminal split

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -13,6 +13,10 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+-- Create new terminal
+vim.keymap.set('n', '<leader>te', ':split | terminal<CR>')
+
+
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
 
