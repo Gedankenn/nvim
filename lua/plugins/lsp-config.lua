7ec564095f6ca82ceb42571357a5992c7b2f438b
fabio <gedankenn@gmail.com>
2024-11-29 07:46:54 -0300

    remove tsserver

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,9 +20,6 @@ return {
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
       lspconfig.solargraph.setup({
         capabilities = capabilities
       })
