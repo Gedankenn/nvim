cde4212dc1c6502a69b094cbef0e427f01020f57
FabioStella <gedankenn@gmail.com>
2025-01-15 07:38:53 -0300

    update versions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
   },
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
+    --tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim", "nvim-lua/popup.nvim", "BurntSushi/ripgrep" },
     config = function()
       require("telescope").setup({
